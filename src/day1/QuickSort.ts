c54e8e5150bfdfc856d51d30150c80cc1fc7b150
Scott Peterson <scott@headway.io>
2022-12-07 16:10:38 -0500

    quicksort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,35 @@
-export default function quick_sort(arr: number[]): void {
+function quickSort(arr: number[], lo: number, hi: number): void {
+    if (lo >= hi) {
+        return;
+    }
 
+    const pivotIndex = partition(arr, lo, hi);
+
+    quickSort(arr, lo, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, hi);
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+
+    let index = lo - 1;
+
+    for (let i = lo; i < hi; ++i) {
+        if (arr[i] <= pivot) {
+            index++;
+            const temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+        }
+    }
+
+    index++;
+    arr[hi] = arr[index];
+    arr[index] = pivot;
+
+    return index;
+}
+
+export default function quick_sort(arr: number[]): void {
+    quickSort(arr, 0, arr.length - 1);
 }
